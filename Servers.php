1cdef6616079fa8a75942e5fb944b15089d84f75
0xChloe <chloe@catastrophesoftware.com>
2023-12-28 23:36:04 +0000

    Changed URLS/IPs etc

--- Servers.php
+++ Servers.php
@@ -30,7 +30,7 @@
 		<th>Game Name</th>
 		<th>Players</th>
 		<th>Map</th>
-		<th>Direct Connect</th>
+		<th>Direct Connect (No longer working for some servers, Steam issue)</th>
 	</tr>
 	<tr>
 		<td><!-- <a id="info" href="KF2.php"> -->
@@ -69,223 +69,6 @@
 			}
 			?>
 	<tr>
-		<td><!-- <a id="info" href="KF2.php"> -->
-		<?php if ($PZInfo ['HostName'] != null)
-			{
-				echo "<a href=PZ1.php>";
-				print_r($PZInfo ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> Project Zomboid </p>	</td>";
-				echo "<td>";
-				print_r($PZInfo ['Players']);
-				echo "/";
-				print_r($PZInfo ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($PZInfo ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(PZADDR);
-				echo ":";
-				print(PZPORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("Project Zomboid #1");
-				echo "</td>";
-				echo "<td><p> Project Zomboid </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>
-/*	<tr>
-		<td><!-- <a id="info" href="CSGO1.php"> -->
-		<?php if ($CSGO1Info ['HostName'] != null)
-			{
-				echo "<a href=CSGO1.php>";
-				print_r($CSGO1Info ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> Counter-Strike: Global Offensive </p>	</td>";
-				echo "<td>";
-				print_r($CSGO1Info ['Players']);
-				echo "/";
-				print_r($CSGO1Info ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($CSGO1Info ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(CSGO1ADDR);
-				echo ":";
-				print(CSGO1PORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("CSGO #1");
-				echo "</td>";
-				echo "<td><p> Counter-Strike: Global Offensive </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>
-*/
-		<tr>	
-		<td><!-- <a id="info" href="ARK1.php"> -->
-		<?php if ($ARK1Info ['HostName'] != null)
-			{
-				echo "<a href=ARK1.php>";
-				print_r($ARK1Info ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td>";
-				print_r($ARK1Info ['Players']);
-				echo "/";
-				print_r($ARK1Info ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($ARK1Info ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(ARK1ADDR);
-				echo ":";
-				print(ARK1PORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("ARKSERVER #1");
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>
-	<tr>	
-		<td><!-- <a id="info" href="ARK2.php"> -->
-		<?php if ($ARK2Info ['HostName'] != null)
-			{
-				echo "<a href=ARK2.php>";
-				print_r($ARK2Info ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td>";
-				print_r($ARK2Info ['Players']);
-				echo "/";
-				print_r($ARK2Info ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($ARK2Info ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(ARK2ADDR);
-				echo ":";
-				print(ARK2PORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("ARKSERVER #2");
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>
-	<tr>	
-		<td><!-- <a id="info" href="ARK3.php"> -->
-		<?php if ($ARK3Info ['HostName'] != null)
-			{
-				echo "<a href=ARK3.php>";
-				print_r($ARK3Info ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td>";
-				print_r($ARK3Info ['Players']);
-				echo "/";
-				print_r($ARK3Info ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($ARK3Info ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(ARK3ADDR);
-				echo ":";
-				print(ARK3PORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("ARKSERVER #2");
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>
-<tr>	
-		<td><!-- <a id="info" href="ARK4.php"> -->
-		<?php if ($ARK4Info ['HostName'] != null)
-			{
-				echo "<a href=ARK4.php>";
-				print_r($ARK4Info ['HostName']);
-				echo "</a>";
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td>";
-				print_r($ARK4Info ['Players']);
-				echo "/";
-				print_r($ARK4Info ['MaxPlayers']);
-				echo "</td>";
-				echo "<td>";
-				print_r($ARK4Info ['Map']); 
-				echo "</td>";
-				echo "<td>";
-				echo "<a href=steam://connect/";
-				print(ARK4ADDR);
-				echo ":";
-				print(ARK4PORT);
-				echo ">";
-				echo "CONNECT</a></td>";
-				echo "</tr>";
-			}
-			else 
-			{
-				print ("ARKSERVER #2");
-				echo "</td>";
-				echo "<td><p> ARK: Survival Evolved </p>	</td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >ERROR! </p> </td>";
-				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
-			}
-			?>	
-	<tr>	
 		<td><!-- <a id="info" href="INS1.php"> -->
 		<?php if ($INS1Info ['HostName'] != null)
 			{
@@ -321,6 +104,78 @@
 				echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
 			}
 			?>
+	<tr>
+		<td><!-- <a id="info" href="INS1.php"> -->
+			<?php if ($PZCasualInfo ['HostName'] != null)
+				{
+					echo "<a href=INS1.php>";
+					print_r($PZCasualInfo ['HostName']);
+					echo "</a>";
+					echo "</td>";
+					echo "<td><p> Project Zomboid </p>	</td>";
+					echo "<td>";
+					print_r($PZCasualInfo ['Players']);
+					echo "/";
+					print_r($PZCasualInfo ['MaxPlayers']);
+					echo "</td>";
+					echo "<td> Muldraugh";
+					//print_r($PZCasualInfo ['Map']); 
+					echo "</td>";
+					echo "<td>";
+					echo "<a href=steam://connect/";
+					print(PZCasualADDR);
+					echo ":";
+					print(PZCasualPORT);
+					echo ">";
+					echo "CONNECT</a></td>";
+					echo "</tr>";
+				}
+				else 
+				{
+					print ("Project Zomboid Casual");
+					echo "</td>";
+					echo "<td><p> Project Zomboid </p>	</td>";
+					echo "<td> <p id=\"error\" >ERROR! </p> </td>";
+					echo "<td> <p id=\"error\" >ERROR! </p> </td>";
+					echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
+				}
+				?>
+	<tr>
+		<td><!-- <a id="info" href="INS1.php"> -->
+			<?php if ($PZHardcoreInfo ['HostName'] != null)
+				{
+					echo "<a href=INS1.php>";
+					print_r($PZHardcoreInfo ['HostName']);
+					echo "</a>";
+					echo "</td>";
+					echo "<td><p> Project Zomboid </p>	</td>";
+					echo "<td>";
+					print_r($PZHardcoreInfo ['Players']);
+					echo "/";
+					print_r($PZHardcoreInfo ['MaxPlayers']);
+					echo "</td>";
+					echo "<td> Muldraugh";
+					// print_r($PZHardcoreInfo ['Map']); 
+					echo "</td>";
+					echo "<td>";
+					echo "<p>";
+					print(PZHardcoreADDR);
+					echo ":";
+					print(PZHardcorePORT);
+					echo "</p>";
+					echo "</td>";
+					echo "</tr>";
+				}
+				else 
+				{
+					print ("Project Zomboid Hardcore");
+					echo "</td>";
+					echo "<td><p> Project Zomboid </p>	</td>";
+					echo "<td> <p id=\"error\" >ERROR! </p> </td>";
+					echo "<td> <p id=\"error\" >ERROR! </p> </td>";
+					echo "<td> <p id=\"error\" >OFFLINE! </p> </td> </tr>";
+				}
+				?>
 
 </table>
 	<body id="uptime">
--- phpcall.php
+++ phpcall.php
@@ -1,10 +1,12 @@
 <?php
 	require __DIR__ . '/SourceQuery/bootstrap.php';
-	
+	$PublicIP = "174.126.235.27";
 	use xPaw\SourceQuery\SourceQuery;
 	//LOOP1 - KF2
 	// Edit this ->
-	define( 'KFADDR', 'public.midnightmonsters.net' );
+
+	
+	define( 'KFADDR', $PublicIP );
 	define( 'KFPORT', 27015 );
 	define( 'KFTIMEOUT',     1 );
 	define( 'KFENGINE',      SourceQuery::SOURCE );
@@ -36,7 +38,7 @@
 	}
 
 	// Edit this ->
-	define( 'PZADDR', 'public.midnightmonsters.net' );
+	define( 'PZADDR', $PublicIP );
 	define( 'PZPORT', 16000 );
 	define( 'PZTIMEOUT',     1 );
 	define( 'PZENGINE',      SourceQuery::SOURCE );
@@ -67,196 +69,196 @@
 		$PZQuery->Disconnect( );
 	}
 	//LOOP3 - CSGO1			
-	define( 'CSGO1ADDR', 'public.midnightmonsters.net' );
-	define( 'CSGO1PORT', 16006 );
-	define( 'CSGO1TIMEOUT',     1 );
-	define( 'CSGO1ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// define( 'CSGO1ADDR', 'public.midnightmonsters.net' );
+	// define( 'CSGO1PORT', 16006 );
+	// define( 'CSGO1TIMEOUT',     1 );
+	// define( 'CSGO1ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$CSGO1Query = new SourceQuery( );
+	// $CSGO1Query = new SourceQuery( );
 
-	$CSGO1Info    = [];
-	$CSGO1Rules   = [];
-	$CSGO1Players = [];
-	$CSGO1Exception = null;
+	// $CSGO1Info    = [];
+	// $CSGO1Rules   = [];
+	// $CSGO1Players = [];
+	// $CSGO1Exception = null;
 
-	try
-	{
-		$CSGO1Query->Connect( CSGO1ADDR, CSGO1PORT, CSGO1TIMEOUT, CSGO1ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$CSGO1Query->Connect( CSGO1ADDR, CSGO1PORT, CSGO1TIMEOUT, CSGO1ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$CSGO1Info    = $CSGO1Query->GetInfo( );
-		$CSGO1Players = $CSGO1Query->GetPlayers( );
-		$CSGO1Rules   = $CSGO1Query->GetRules( );
-	}
-	catch( Exception $CSGO1e )
-	{
-		$CSGO1Exception = $CSGO1e;
-	}
-	finally
-	{
-		$CSGO1Query->Disconnect( );
-	}
+	// 	$CSGO1Info    = $CSGO1Query->GetInfo( );
+	// 	$CSGO1Players = $CSGO1Query->GetPlayers( );
+	// 	$CSGO1Rules   = $CSGO1Query->GetRules( );
+	// }
+	// catch( Exception $CSGO1e )
+	// {
+	// 	$CSGO1Exception = $CSGO1e;
+	// }
+	// finally
+	// {
+	// 	$CSGO1Query->Disconnect( );
+	// }
 
 
-	/* define( 'CSGO2ADDR', 'public.midnightmonsters.net' );
-	define( 'CSGO2PORT', 16011 );
-	define( 'CSGO2TIMEOUT',     1 );
-	define( 'CSGO2ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// /* define( 'CSGO2ADDR', 'public.midnightmonsters.net' );
+	// define( 'CSGO2PORT', 16011 );
+	// define( 'CSGO2TIMEOUT',     1 );
+	// define( 'CSGO2ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$CSGO2Query = new SourceQuery( );
+	// $CSGO2Query = new SourceQuery( );
 
-	$CSGO2Info    = [];
-	$CSGO2Rules   = [];
-	$CSGO2Players = [];
-	$CSGO2Exception = null;
+	// $CSGO2Info    = [];
+	// $CSGO2Rules   = [];
+	// $CSGO2Players = [];
+	// $CSGO2Exception = null;
 
-	try
-	{
-		$CSGO2Query->Connect( CSGO2ADDR, CSGO2PORT, CSGO2TIMEOUT, CSGO2ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$CSGO2Query->Connect( CSGO2ADDR, CSGO2PORT, CSGO2TIMEOUT, CSGO2ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$CSGO2Info    = $CSGO2Query->GetInfo( );
-		$CSGO2Players = $CSGO2Query->GetPlayers( );
-		$CSGO2Rules   = $CSGO2Query->GetRules( );
-	}
-	catch( Exception $CSGO2e )
-	{
-		$CSGO2Exception = $CSGO2e;
-	}
-	finally
-	{
-		$CSGO2Query->Disconnect( );
-	}
+	// 	$CSGO2Info    = $CSGO2Query->GetInfo( );
+	// 	$CSGO2Players = $CSGO2Query->GetPlayers( );
+	// 	$CSGO2Rules   = $CSGO2Query->GetRules( );
+	// }
+	// catch( Exception $CSGO2e )
+	// {
+	// 	$CSGO2Exception = $CSGO2e;
+	// }
+	// finally
+	// {
+	// 	$CSGO2Query->Disconnect( );
+	// }
 
-	*/
-	define( 'ARK1ADDR', 'public.midnightmonsters.net' );
-	define( 'ARK1PORT', 16016 );
-	define( 'ARK1TIMEOUT',     1 );
-	define( 'ARK1ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// */
+	// define( 'ARK1ADDR', 'public.midnightmonsters.net' );
+	// define( 'ARK1PORT', 16016 );
+	// define( 'ARK1TIMEOUT',     1 );
+	// define( 'ARK1ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$ARK1Query = new SourceQuery( );
+	// $ARK1Query = new SourceQuery( );
 
-	$ARK1Info    = [];
-	$ARK1Rules   = [];
-	$ARK1Players = [];
-	$ARK1Exception = null;
+	// $ARK1Info    = [];
+	// $ARK1Rules   = [];
+	// $ARK1Players = [];
+	// $ARK1Exception = null;
 
-	try
-	{
-		$ARK1Query->Connect( ARK1ADDR, ARK1PORT, ARK1TIMEOUT, ARK1ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$ARK1Query->Connect( ARK1ADDR, ARK1PORT, ARK1TIMEOUT, ARK1ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$ARK1Info    = $ARK1Query->GetInfo( );
-		$ARK1Players = $ARK1Query->GetPlayers( );
-		$ARK1Rules   = $ARK1Query->GetRules( );
-	}
-	catch( Exception $ARK1e )
-	{
-		$ARK1Exception = $ARK1e;
-	}
-	finally
-	{
-		$ARK1Query->Disconnect( );
-	}
+	// 	$ARK1Info    = $ARK1Query->GetInfo( );
+	// 	$ARK1Players = $ARK1Query->GetPlayers( );
+	// 	$ARK1Rules   = $ARK1Query->GetRules( );
+	// }
+	// catch( Exception $ARK1e )
+	// {
+	// 	$ARK1Exception = $ARK1e;
+	// }
+	// finally
+	// {
+	// 	$ARK1Query->Disconnect( );
+	// }
 
 
-	define( 'ARK2ADDR', 'public.midnightmonsters.net' );
-	define( 'ARK2PORT', 16027 );
-	define( 'ARK2TIMEOUT',     1 );
-	define( 'ARK2ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// define( 'ARK2ADDR', 'public.midnightmonsters.net' );
+	// define( 'ARK2PORT', 16027 );
+	// define( 'ARK2TIMEOUT',     1 );
+	// define( 'ARK2ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$ARK2Query = new SourceQuery( );
+	// $ARK2Query = new SourceQuery( );
 
-	$ARK2Info    = [];
-	$ARK2Rules   = [];
-	$ARK2Players = [];
-	$ARK2Exception = null;
+	// $ARK2Info    = [];
+	// $ARK2Rules   = [];
+	// $ARK2Players = [];
+	// $ARK2Exception = null;
 
-	try
-	{
-		$ARK2Query->Connect( ARK2ADDR, ARK2PORT, ARK2TIMEOUT, ARK2ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$ARK2Query->Connect( ARK2ADDR, ARK2PORT, ARK2TIMEOUT, ARK2ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$ARK2Info    = $ARK2Query->GetInfo( );
-		$ARK2Players = $ARK2Query->GetPlayers( );
-		$ARK2Rules   = $ARK2Query->GetRules( );
-	}
-	catch( Exception $ARK2e )
-	{
-		$ARK2Exception = $ARK2e;
-	}
-	finally
-	{
-		$ARK2Query->Disconnect( );
-	}
+	// 	$ARK2Info    = $ARK2Query->GetInfo( );
+	// 	$ARK2Players = $ARK2Query->GetPlayers( );
+	// 	$ARK2Rules   = $ARK2Query->GetRules( );
+	// }
+	// catch( Exception $ARK2e )
+	// {
+	// 	$ARK2Exception = $ARK2e;
+	// }
+	// finally
+	// {
+	// 	$ARK2Query->Disconnect( );
+	// }
 
-	define( 'ARK3ADDR', 'public.midnightmonsters.net' );
-	define( 'ARK3PORT', 16038 );
-	define( 'ARK3TIMEOUT',     1 );
-	define( 'ARK3ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// define( 'ARK3ADDR', 'public.midnightmonsters.net' );
+	// define( 'ARK3PORT', 16038 );
+	// define( 'ARK3TIMEOUT',     1 );
+	// define( 'ARK3ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$ARK3Query = new SourceQuery( );
+	// $ARK3Query = new SourceQuery( );
 
-	$ARK3Info    = [];
-	$ARK3Rules   = [];
-	$ARK3Players = [];
-	$ARK3Exception = null;
+	// $ARK3Info    = [];
+	// $ARK3Rules   = [];
+	// $ARK3Players = [];
+	// $ARK3Exception = null;
 
-	try
-	{
-		$ARK3Query->Connect( ARK3ADDR, ARK3PORT, ARK3TIMEOUT, ARK3ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$ARK3Query->Connect( ARK3ADDR, ARK3PORT, ARK3TIMEOUT, ARK3ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$ARK3Info    = $ARK3Query->GetInfo( );
-		$ARK3Players = $ARK3Query->GetPlayers( );
-		$ARK3Rules   = $ARK3Query->GetRules( );
-	}
-	catch( Exception $ARK3e )
-	{
-		$ARK3Exception = $ARK3e;
-	}
-	finally
-	{
-		$ARK3Query->Disconnect( );
-	}
+	// 	$ARK3Info    = $ARK3Query->GetInfo( );
+	// 	$ARK3Players = $ARK3Query->GetPlayers( );
+	// 	$ARK3Rules   = $ARK3Query->GetRules( );
+	// }
+	// catch( Exception $ARK3e )
+	// {
+	// 	$ARK3Exception = $ARK3e;
+	// }
+	// finally
+	// {
+	// 	$ARK3Query->Disconnect( );
+	// }
 
-	define( 'ARK4ADDR', 'public.midnightmonsters.net' );
-	define( 'ARK4PORT', 16045 );
-	define( 'ARK4TIMEOUT',     1 );
-	define( 'ARK4ENGINE',      SourceQuery::SOURCE );
-	// Edit this <-
+	// define( 'ARK4ADDR', 'public.midnightmonsters.net' );
+	// define( 'ARK4PORT', 16045 );
+	// define( 'ARK4TIMEOUT',     1 );
+	// define( 'ARK4ENGINE',      SourceQuery::SOURCE );
+	// // Edit this <-
 
-	$ARK4Query = new SourceQuery( );
+	// $ARK4Query = new SourceQuery( );
 
-	$ARK4Info    = [];
-	$ARK4Rules   = [];
-	$ARK4Players = [];
-	$ARK4Exception = null;
+	// $ARK4Info    = [];
+	// $ARK4Rules   = [];
+	// $ARK4Players = [];
+	// $ARK4Exception = null;
 
-	try
-	{
-		$ARK4Query->Connect( ARK4ADDR, ARK4PORT, ARK4TIMEOUT, ARK4ENGINE );
-		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+	// try
+	// {
+	// 	$ARK4Query->Connect( ARK4ADDR, ARK4PORT, ARK4TIMEOUT, ARK4ENGINE );
+	// 	//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
 		
-		$ARK4Info    = $ARK4Query->GetInfo( );
-		$ARK4Players = $ARK4Query->GetPlayers( );
-		$ARK4Rules   = $ARK4Query->GetRules( );
-	}
-	catch( Exception $ARK4e )
-	{
-		$ARK4Exception = $ARK4e;
-	}
-	finally
-	{
-		$ARK4Query->Disconnect( );
-	}
+	// 	$ARK4Info    = $ARK4Query->GetInfo( );
+	// 	$ARK4Players = $ARK4Query->GetPlayers( );
+	// 	$ARK4Rules   = $ARK4Query->GetRules( );
+	// }
+	// catch( Exception $ARK4e )
+	// {
+	// 	$ARK4Exception = $ARK4e;
+	// }
+	// finally
+	// {
+	// 	$ARK4Query->Disconnect( );
+	// }
 
-	define( 'INS1ADDR', 'public.midnightmonsters.net' );
+	define( 'INS1ADDR', $PublicIP );
 	define( 'INS1PORT', 16035 );
 	define( 'INS1TIMEOUT',     1 );
 	define( 'INS1ENGINE',      SourceQuery::SOURCE );
@@ -285,6 +287,73 @@
 	finally
 	{
 		$INS1Query->Disconnect( );
+	}
+
+	
+
+
+
+
+	define( 'PZCasualADDR', $PublicIP );
+	define( 'PZCasualPORT', 16053 );
+	define( 'PZCasualTIMEOUT',     1 );
+	define( 'PZCasualENGINE',      SourceQuery::SOURCE );
+	// Edit this <-
+	//LOOP2 - PZServer
+	$PZCasualQuery = new SourceQuery( );
+
+	$PZCasualInfo    = [];
+	$PZCasualRules   = [];
+	$PZCasualPlayers = [];
+	$PZCasualException = null;
+
+	try
+	{
+		$PZCasualQuery->Connect( PZCasualADDR, PZCasualPORT, PZCasualTIMEOUT, PZCasualENGINE );
+		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+		
+		$PZCasualInfo    = $PZCasualQuery->GetInfo( );
+		$PZCasualPlayers = $PZCasualQuery->GetPlayers( );
+		$PZCasualRules   = $PZCasualQuery->GetRules( );
+	}
+	catch( Exception $PZCasuale )
+	{
+		$PZCasualException = $PZCasuale;
+	}
+	finally
+	{
+		$PZCasualQuery->Disconnect( );
+	}
+
+	define( 'PZHardcoreADDR', $PublicIP );
+	define( 'PZHardcorePORT', 16034 );
+	define( 'PZHardcoreTIMEOUT',     1 );
+	define( 'PZHardcoreENGINE',      SourceQuery::SOURCE );
+	// Edit this <-
+	//LOOP2 - PZServer
+	$PZHardcoreQuery = new SourceQuery( );
+
+	$PZHardcoreInfo    = [];
+	$PZHardcoreRules   = [];
+	$PZHardcorePlayers = [];
+	$PZHardcoreException = null;
+
+	try
+	{
+		$PZHardcoreQuery->Connect( PZHardcoreADDR, PZHardcorePORT, PZHardcoreTIMEOUT, PZHardcoreENGINE );
+		//$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+		
+		$PZHardcoreInfo    = $PZHardcoreQuery->GetInfo( );
+		$PZHardcorePlayers = $PZHardcoreQuery->GetPlayers( );
+		$PZHardcoreRules   = $PZHardcoreQuery->GetRules( );
+	}
+	catch( Exception $PZHardcoree )
+	{
+		$PZHardcoreException = $PZHardcoree;
+	}
+	finally
+	{
+		$PZHardcoreQuery->Disconnect( );
 	}
 
 	?>
